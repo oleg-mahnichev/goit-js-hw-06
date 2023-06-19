20abda969ecfd07ae6a0af947e1dd4669601011c
oleg-mahnichev <124716395+oleg-mahnichev@users.noreply.github.com>
2023-06-19 12:41:50 +0200

    fix_moments_in_tasks

--- js/task-01.js
+++ js/task-01.js
@@ -4,8 +4,8 @@ const categoryCount = items.length; // find count of categories
 console.log('Number of categories:', categoryCount);// display Number of categories
 
 items.forEach((item) => {
-    const Header = item.querySelector('h2').textContent;// take text from h2
+    const header = item.querySelector('h2').textContent;// take text from h2
     const elementsCount = item.querySelectorAll('li').length; //take count of element
-    console.log('Category:', Header); // display header
+    console.log('Category:', header); // display header
     console.log('Elements:', elementsCount); // display count of element this category
 });
--- js/task-02.js
+++ js/task-02.js
@@ -6,13 +6,26 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
-const ingredientsList = document.querySelector('ul#ingredients');
 
-ingredients.forEach((ingredient) => {
-  const listItem = document.createElement('li');
-  listItem.textContent = ingredient;
-  listItem.classList.add('item');
-  ingredientsList.appendChild(listItem);
-  console.log(ingredient);
+// const ingredientsList = document.querySelector('#ingredients');
+
+// ingredients.forEach((ingredient) => {
+//   const listItem = document.createElement('li');
+//   listItem.textContent = ingredient;
+//   listItem.classList.add('item');
+//   ingredientsList.appendChild(listItem);
+// });
+
+// console.log('Результат:');
+// console.log(ingredientsList.innerHTML);
+
+const ul = document.querySelector('#ingredients');
+
+const items = ingredients.map(ingredient => {
+  const li = document.createElement('li');
+  li.classList.add('item');
+  li.textContent = ingredient;
+  return li;
 });
-
+console.log(items);
+ul.append(...items);
